a4351cddff57c5888ca157a9f643b73fd87b232e
MilesYang <miles_yang@foxmail.com>
2017-12-26 17:17:37 +0800

    删除注释
    删除了不必要的注释

--- source/KrWindow.cpp
+++ source/KrWindow.cpp
@@ -9,7 +9,6 @@ namespace KrUI
 		m_bMouseDown = false;
 	}
 
-
 	LPCWSTR KrWindow::GetWindowName()
 	{
 		return m_lpName;
@@ -26,12 +25,10 @@ namespace KrUI
 		m_hwnd = hwnd;
 	}
 
-
 	HWND KrWindow::GetHWND()
 	{
 		return m_hwnd;
 	}
-
 
 	KrUIBase* KrWindow::AddControl(KrUIType t, LPCWSTR lpName, int x, int y, int width, int height)
 	{
@@ -48,16 +45,12 @@ namespace KrUI
 		return pui;
 	}
 
-
 	void KrWindow::SetRect(RECT* pRect)
 	{
 		KrUIBase::SetRect(pRect);
 		if (IsCreated())MoveWindow(m_hwnd, m_rect.left, m_rect.top, m_rect.right - m_rect.left, m_rect.bottom - m_rect.top, TRUE);
 
 	}
-
-
-
 
 	void KrWindow::SetStyle(DWORD dwStyle)
 	{
@@ -70,18 +63,12 @@ namespace KrUI
 		return m_dwStyle;
 	}
 
-
-
-
-
-
 	void KrWindow::SetX(UINT x)
 	{
 		KrUIBase::SetX(x);
 		if (IsCreated())MoveWindow(m_hwnd, m_rect.left, m_rect.top, m_rect.right - m_rect.left, m_rect.bottom - m_rect.top, TRUE);
 
 	}
-
 
 	void KrWindow::SetY(UINT y)
 	{
@@ -96,23 +83,17 @@ namespace KrUI
 		if (IsCreated())MoveWindow(m_hwnd, m_rect.left, m_rect.top, m_rect.right - m_rect.left, m_rect.bottom - m_rect.top, TRUE);
 	}
 
-
-
 	void KrWindow::SetWidth(UINT width)
 	{
 		KrUIBase::SetWidth(width);
 		if (IsCreated())MoveWindow(m_hwnd, m_rect.left, m_rect.top, m_rect.right - m_rect.left, m_rect.bottom - m_rect.top, TRUE);
-
 	}
-
 
 	void KrWindow::SetHeight(UINT height)
 	{
 		KrUIBase::SetHeight(height);
 		if (IsCreated())MoveWindow(m_hwnd, m_rect.left, m_rect.top, m_rect.right - m_rect.left, m_rect.bottom - m_rect.top, TRUE);
-
 	}
-
 
 	void KrWindow::Show()
 	{
@@ -122,9 +103,7 @@ namespace KrUI
 			KrUIBase::Show();
 			UpdateWindow(m_hwnd);
 		}
-
 	}
-
 
 	void KrWindow::Hide()
 	{
@@ -135,8 +114,6 @@ namespace KrUI
 		}
 	}
 
-
-
 	bool KrWindow::IsCreated()
 	{
 		if (m_hwnd == NULL)
@@ -146,32 +123,8 @@ namespace KrUI
 		return true;
 	}
 
-
-
-
-
-	// 	void KrWindow::RegMsg(UINT msg, MSGPROC proc)
-	// 	{
-	// 		m_MsgProcMap.insert(multimap<UINT, MSGPROC>::value_type(msg, proc));
-	// 	}
-
-	// 	void KrWindow::RemoveMsgProc(MSGPROC proc)
-	// 	{
-	// 		auto ret = m_MsgProcMap.end();
-	// 		//不安全，上层存在m_MsgFuncMap的遍历
-	// 		for (auto it = m_MsgProcMap.begin(); it != m_MsgProcMap.end(); ++it)
-	// 		{
-	// 			if (*it->second==proc)
-	// 			{
-	// 				ret = it;
-	// 			}
-	// 		}
-	// 		m_MsgProcMap.erase(ret);
-	// 	}
-
 	LRESULT  KrWindow::HandleMessage(UINT Message, WPARAM wParam, LPARAM lParam)
 	{
-		//
 
 #ifdef _DEBUG
 		//cout << "message:" << Message << ",wParam:" << wParam << ",lParam:" << lParam << endl;
@@ -204,8 +157,6 @@ namespace KrUI
 		KrMessageHandler::HandleMessage(Message, wParam, lParam);
 		return DefWindowProc(m_hwnd, Message, wParam, lParam);
 	}
-
-
 	// 			case WM_DESTROY:
 	// 				//本窗口被销毁时，检查程序是否存在窗口
 	// 				KrUIManager::GetpKrUIManager()->DeleteWindow(this);
