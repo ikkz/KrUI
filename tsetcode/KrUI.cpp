6122dec6a62c86c0ffbe32097fbe323f681d5419
MilesYang <miles_yang@foxmail.com>
2018-01-04 16:48:26 +0800

    ...

--- tsetcode/KrUI.cpp
+++ tsetcode/KrUI.cpp
@@ -1,21 +1,17 @@
-// KrUI.cpp : 定义应用程序的入口点。
-//typedef LRESULT(*MSGPROC) (KrMessageHandler* pKrMessageHandler, WPARAM wParam, LPARAM lParam);
-#include "stdafx.h"
-#include "KrUI.h"
 #include "source/KrCore.h"
-#include <windows.h>
 using namespace KrUI;
+KrUIManager* p = KrUI::KrUIManager::GetpKrUIManager();
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	KrUIManager* p = KrUI::KrUIManager::GetpKrUIManager();
 	if (!p->Initialize(hInstance))return 0;
-	KrWindow* pw = p->AddWindow(L"欢迎使用KrUI", 100, 100,500,300);
+	KrWindow* pw = p->AddWindow(L"这不是一个窗口", 100, 100, 350, 200);
 	pw->Show();
-	KrButton* pb = dynamic_cast<KrButton*>(pw->AddControl(KrButton_t, L"Button1", 70, 135, 100, 25));
+	KrButton* pb = dynamic_cast<KrButton*>(pw->AddControl(KrButton_t, L"这才不是按钮", 100, 50, 150, 25));
 	pb->Show();
-	KrButton* pb1 = dynamic_cast<KrButton*>(pw->AddControl(KrButton_t, L"Button2", 300, 135, 100, 25));
+	KrEdit* pb1 = dynamic_cast<KrEdit*>(pw->AddControl(KrEdit_t, L"瞎jb填反正也不显示", 100, 100, 150, 25));
+	pb1->SetText(L"这也不是文本框");
 	pb1->Show();
-	KrLabel* pl1 = dynamic_cast<KrLabel*>(pw->AddControl(KrLabel_t, L"第8章 VBA程序设计概述（一）宏与VBA、VBA编辑环境、对象、属性、方法和事件、用户窗体与控件", 70, 160, 150, 150));
+	KrLabel* pl1 = dynamic_cast<KrLabel*>(pw->AddControl(KrLabel_t, L"这个当然也不是标签", 100, 150, 200, 25));
 	pl1->Show();
 	return p->MessageLoop();
 }
