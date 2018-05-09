1fb332b1aa32cbc7bdb380e687e45f9d82f153c6
MilesYang <miles_yang@foxmail.com>
2018-05-09 23:08:16 +0800

    ..

---
+++ KrUI/KrUI.h
@@ -0,0 +1,13 @@
+/*********************************************************
+Author:MilesYang
+Mail:1961464399@qq.com
+CurrentTime:2018-4-15
+FileName:KrUI.h
+*********************************************************/
+
+#ifndef _KRUI_H_
+#define _KRUI_H_
+
+#include "headers/KrCore.h"
+
+#endif
